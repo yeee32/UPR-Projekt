edb884a6c082bce574f3089affdecc01edcfb52b
yeee32 <97116766+yeee32@users.noreply.github.com>
2025-01-01 20:04:54 +0100

    Update match.h

--- match.h
+++ match.h
@@ -1,8 +1,14 @@
 #pragma once
 #include "player.h"
 
+typedef struct{
+    int kills;
+    int assists;
+    int deaths;
+}TempKAD;
+
 void readIds(FILE* matchFile, int* IDsArr);
-void setStats(FILE* matchFIle, int* kills, int* assists, int* deaths);
+void setStats(FILE* matchFIle, TempKAD* teamStats);
 int sameIDinTeam(int* ids);
 int hasDuplicateIDs(int* redIDs, int* blueIDs);
 void processMatch(Player* players, int playerCount, FILE* matchFile);
